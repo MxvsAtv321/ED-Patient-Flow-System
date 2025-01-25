3b9b700451d86fe8a900a27d535fb0e48f15bf8e
MiikaMatias <miikapiiparinen24@gmail.com>
2025-01-25 16:08:01 -0500

    backend: quality of life!

--- backend/src/hospital_data/expected_time.py
+++ backend/src/hospital_data/expected_time.py
@@ -1,11 +1,12 @@
+from flask import current_app
 from hospital_data.hospital_getters import get_patient_data, get_hospital_state
 
-def compute_expected_time_seconds(patient_id: int) -> str:
+def compute_expected_time_seconds(patient_id: str) -> str:
     triage_cat = str(get_patient_data(patient_id)['triage_category'])
-    print("tg:", triage_cat)
+    current_app.logger.info(f"fetched triage category for patient {patient_id}: {triage_cat}")
     wait_time_for_cat = get_hospital_state()['averageWaitTimes'][triage_cat]
-    print("wt:",wait_time_for_cat)
-    return str(wait_time_for_cat)
+    current_app.logger.info(f"wait time for {patient_id}: {wait_time_for_cat}")
+    return wait_time_for_cat
 
 if __name__ == "__main__":
     compute_expected_time_seconds(1)
--- backend/src/hospital_data/hospital_getters.py
+++ backend/src/hospital_data/hospital_getters.py
@@ -5,7 +5,7 @@ from typing import Dict, Any
 def get_hospital_state() -> Dict[str, Any]:
     return requests.get(CURRENT_HOSPITAL_STATE_URL).json()
 
-def get_patient_data(patient_id: int) -> Dict[str, Any]:
+def get_patient_data(patient_id: str) -> Dict[str, Any]:
     return requests.get(SPECIFIC_PATIENT_INFO_URL.format(patient_id=patient_id)).json()
 
 def get_queue_data() -> Dict[str, Any]:
