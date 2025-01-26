03c44decf161177667748b6a317a5a83c3ec9073
MiikaMatias <miikapiiparinen24@gmail.com>
2025-01-25 20:55:38 -0500

    backend: add data to patient/

--- backend/src/app.py
+++ backend/src/app.py
@@ -4,8 +4,9 @@ from config import BACKEND_PORT
 import logging
 
 from hospital_data.expected_time import compute_expected_time_seconds
-from hospital_data.queue_data import get_queue_stats
 from hospital_data.create_db import save_hospital_data
+from hospital_data.hospital_api import get_patient_data, get_all_patient_data
+from hospital_data.queue_data import get_queue_stats
 
 app = Flask(__name__)
 swagger = Swagger(app)
@@ -48,16 +49,20 @@ def get_wait_times(patient_id: str):
     """
 
     queue_data = get_queue_stats(patient_id)
-
+    patient_data = get_patient_data(patient_id)
     expected_time = compute_expected_time_seconds(patient_id)
     if expected_time == -1:
         return {"error": f"no such patient_id {patient_id}"}
 
     return jsonify({
+        "arrivalTime": patient_data.arrival_time,
+        "elapsedTime": patient_data.wait_time,
+        "triage": patient_data.triage_category,
         "expectedTime": expected_time,
         "queuePositionLocal": queue_data[0],
         "queuePositionGlobal": queue_data[1],
         "queueMax": queue_data[2],
+        "allPatients": len(get_all_patient_data())
     })
 
 if __name__ == "__main__":
--- backend/src/routes/patient_routes.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint, jsonify, request 
-from src.models.patient import Patient, TriageLevel, PatientStatus 
-from src.extensions import db 
-import uuid 
-from datetime import datetime 
- 
-patient_bp = Blueprint('patient', __name__) 
